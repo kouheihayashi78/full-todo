d059ba09a311bf28f189fbb1050efe85ffdd8c98
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-15 16:02:54 +0900

    add: PrismaClientを使用してデータを全て取得

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import type { Express, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app: Express = express();
-console.log("Expressアプリを作成しました");
-
 const PORT = 8080;
+const prisma = new PrismaClient();
 
-app.get("/allTodos", (req: Request, res:Response) => {
-    return res.send("API通信テスト")
-})
+app.get("/allTodos", async (req: Request, res: Response) => {
+  try {
+    const todoList = await prisma.todo.findMany();
+    return res.json(todoList);
+  } catch (error) {
+    console.error("データベースエラー:", error);
+    return res.status(500).json({ error: "データベースエラーが発生しました" });
+  }
+});
 
 // ローカルサーバー立ち上げ
 app
