51764442049bf155fc93266f35c0b216d80bd690
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-15 15:22:40 +0900

    add: expressでapiの通信テスト、サーバ起動するか確認の記述

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,21 @@
-console.log('hello express');
+import express from "express";
+import type { Express, Request, Response } from "express";
+
+const app: Express = express();
+console.log("Expressアプリを作成しました");
+
+const PORT = 8080;
+
+app.get("/allTodos", (req: Request, res:Response) => {
+    return res.send("API通信テスト")
+})
+
+// ローカルサーバー立ち上げ
+app
+  .listen(PORT, () => {
+    console.log("サーバーが立ち上がりました！");
+    console.log(`http://localhost:${PORT} でアクセスできます`);
+  })
+  .on("error", (error) => {
+    console.error("サーバー起動エラー:", error);
+  });
