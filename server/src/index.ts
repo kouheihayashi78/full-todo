b215a97769e13bf713c1b48cda4b65d2e0f43902
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-16 10:06:49 +0900

    add: データ編集apiを実装

--- server/src/index.ts
+++ server/src/index.ts
@@ -38,6 +38,29 @@ app.post("/createTodo", async (req: Request, res: Response) => {
   }
 });
 
+// todo編集api
+app.put("/updateTodo/:id", async (req: Request, res: Response) => {
+  try {
+    // urlの:idにparams以降の名前を合わせる
+    const id  = Number(req.params.id);
+    const { title, isCompleted } = req.body;
+    const updateTodo = await prisma.todo.update({
+      where: {
+        id
+      },
+      data: {
+        title,
+        isCompleted,
+      }
+    });
+    return res.status(200).json(updateTodo);
+  } catch (error) {
+    console.error("データベースエラー:", error);
+    return res.status(500).json({ error: "データベースエラーが発生しました" + error });
+  }
+});
+
+
 // ローカルサーバー立ち上げ
 app
   .listen(PORT, () => {
